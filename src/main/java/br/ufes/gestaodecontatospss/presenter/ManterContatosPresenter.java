97ba1d0b82499d13011834e662e8ade492ac97ed
R@fa3lP <rafaelprenholato@hotmail.com>
2021-12-05 14:23:58 -0300

    para descartar

--- src/main/java/br/ufes/gestaodecontatospss/presenter/ManterContatosPresenter.java
+++ src/main/java/br/ufes/gestaodecontatospss/presenter/ManterContatosPresenter.java
@@ -224,7 +224,7 @@ public class ManterContatosPresenter {
     }
     
     private boolean telefoneCorreto(String telefone) {
-        return Pattern.matches("^\\([1-9]{2}\\)(?:[2-8]|9[1-9])[0-9]{3}\\-[0-9]{4}$", telefone);
+        return Pattern.matches("^\\([1-9]{2}\\)(?:[2-9]|9[1-9])[0-9]{3}\\-[0-9]{4}$", telefone);
     }
     
     private boolean nomeCorreto(String nome) {
